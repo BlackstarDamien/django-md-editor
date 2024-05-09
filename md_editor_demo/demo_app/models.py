37bd39b86f7ffd50d1017e5c98a8224d02a9c849
BlackstarDamien <damianzylka@gmail.com>
2024-05-09 19:56:20 +0200

    Add demo app related templates and views

--- md_editor_demo/demo_app/models.py
+++ md_editor_demo/demo_app/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Article(models.Model):
     author = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
     content = models.TextField()
+
+    def get_absolute_url(self):
+        return reverse("article_page", kwargs={"id": self.id})
